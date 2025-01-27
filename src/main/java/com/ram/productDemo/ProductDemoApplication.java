453c71bf7eaf892e53b2cdb4f39c26eb65b9ba54
ramanes <ramanes@smartretail.co>
2025-01-27 15:59:46 +0800

    update db credential

--- src/main/java/com/ram/productDemo/ProductDemoApplication.java
+++ src/main/java/com/ram/productDemo/ProductDemoApplication.java
@@ -1,13 +1,24 @@
 package com.ram.productDemo;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductDemoApplication {
 
+	@Value("${Postgres.DATABASE_URL}")
+	private static String databaseUrl;
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProductDemoApplication.class, args);
+	}
+
+	@PostConstruct
+	public void printDatabaseUrl() {
+		System.out.println("Resolved Postgres Database URL: " + databaseUrl);
 	}
 
 }
